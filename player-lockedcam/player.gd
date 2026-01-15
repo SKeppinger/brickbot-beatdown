01ddd6a49c3e7b052b56253876b5a4a53850cb5e
Steven Keppinger <skeppinger18@gmail.com>
2026-01-15 10:51:17 -0500

    it's even smoother now

--- player-lockedcam/player.gd
+++ player-lockedcam/player.gd
@@ -24,8 +24,6 @@ func _physics_process(_delta):
 	if not GameState.paused:
 		## Player Input
 		var input_dir = Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
-		## Camera Lock
-		look_at(target.global_position, up_direction)
 		## Player Movement
 		var direction_facing = (global_position.direction_to(target.global_position) * input_dir.y).normalized()
 		var direction_strafe = (global_position.direction_to(target.global_position).cross(up_direction)) * input_dir.x
@@ -37,3 +35,5 @@ func _physics_process(_delta):
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 		move_and_slide()
+		## Camera Lock
+		look_at(target.global_position, up_direction)
