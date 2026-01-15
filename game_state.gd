3def6e1baaaab683b987a2d94ff4b3ae58bb2af1
Steven Keppinger <skeppinger18@gmail.com>
2026-01-15 08:41:19 -0500

    vertical camera motion for semilocked cam

---
+++ game_state.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var paused = false # Whether the game is paused or not
--- player-semilockedcam/player.gd
+++ player-semilockedcam/player.gd
@@ -3,24 +3,38 @@ class_name PlayerSemilockedCam
 
 const SPEED = 10.0
 const CAM_SENS = 0.001
+const VCAM_RANGE = PI / 4 # This is the maximum vertical camera rotation, in radians
 
 @onready var facing = $CollisionShape3D/FacingDirection
+@onready var camera_pivot = $CameraPivot
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
-	## Camera Movement
-	if event is InputEventMouseMotion:
-		var mouse_motion = -1 * event.screen_relative.x * CAM_SENS
-		rotation += Vector3(0, mouse_motion, 0)
+	if not GameState.paused:
+		## Camera Movement
+		if event is InputEventMouseMotion:
+			var mouse_motion_h = -1 * event.screen_relative.x * CAM_SENS
+			rotation += Vector3(0, mouse_motion_h, 0)
+			var mouse_motion_v = event.screen_relative.y * CAM_SENS
+			camera_pivot.rotation += Vector3(0, 0, mouse_motion_v)
+			if abs(camera_pivot.rotation.z) > VCAM_RANGE:
+				camera_pivot.rotation.z = sign(camera_pivot.rotation.z) * VCAM_RANGE
 		
 	## hi steven, i just quickly put in escape key functionality
-	if event.is_action_pressed("ui_cancel"):
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+	## you are the GOAT, but I'm going to put it in the process function bc otherwise it will trigger as long
+	## as ESC is held down, not just when it's initially pressed! Also going to add a pause/unpause framework
+	## to program defensively
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if not GameState.paused:
+			GameState.paused = true
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
-			Input.MOUSE_MODE_CAPTURED
+			GameState.paused = false
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(_delta):
 	## Player Movement
