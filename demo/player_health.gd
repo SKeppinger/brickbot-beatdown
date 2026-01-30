17fb4e6a3ad61f9a55c4fd90e29a4d3fb51417ce
IFgiht4TheUsers <thedarkone343@gmail.com>
2026-01-29 22:38:46 -0500

    added interface. added player and enemy health bars

---
+++ demo/player_health.gd
@@ -0,0 +1,10 @@
+extends ProgressBar
+
+func _ready():
+  var player_node = get_tree().get_first_node_in_group("player") # get player object on scene tree, actual name may differ
+  if player_node:
+	player_node.connect("player_health", Callable(self, "_on_player_health_changed")) # assumes player has a signal named player_health representing the current health
+	value = player_node.health # initialize progress bar value to player health
+
+func _on_player_health_changed(new_health_value: int):
+  value = new_health_value
